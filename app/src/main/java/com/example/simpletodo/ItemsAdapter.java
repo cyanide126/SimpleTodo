7c045588825eb138cbf6cbd687577190db6b15ea
cyanide126 <cyada2000@gmail.com>
2020-05-31 17:37:32 +0700

    edit task

--- app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -12,16 +12,23 @@ import java.util.List;
 
 // Responsible for displaying data from the model into a row in the recycler view
 public class ItemsAdapter extends  RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+    public interface  OnClickListener{
+        void OnItemClicked(int position);
+    }
+
     public interface OnLongClickListener{
         void OnItemLongClicked(int position);
     }
 
     List<String> items;
     OnLongClickListener longClickListener;
+    OnClickListener clickListener;
 
-    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener) {
+
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener, OnClickListener clickListener) {
         this.items = items;
         this.longClickListener = longClickListener;
+        this.clickListener = clickListener;
     }
 
     @NonNull
@@ -61,6 +68,12 @@ public class ItemsAdapter extends  RecyclerView.Adapter<ItemsAdapter.ViewHolder>
         //Update the view inside of the view holder with the data
         public void bind(String item) {
             tvItem.setText(item);
+            tvItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.OnItemClicked(getAdapterPosition());
+                }
+            });
             tvItem.setOnLongClickListener(new View.OnLongClickListener() {
                 @Override
                 public boolean onLongClick(View v) {
